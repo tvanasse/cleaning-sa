b697d204ef68abaf199ffe5cbfa2853a8f91c384
Tom Vanasse <thomas.j.vanasse@gmail.com>
2020-05-28 11:57:15 -0500

    added export_fig function

--- mffs/mff_convert_to_chanlocs.m
+++
@@ -1,9 +0,0 @@
-function [chanlocs] = mff_convert_to_chanlocs(meta_file)
-    
-    coordinates = mff_import_coordinates(meta_file);
-    sfp=transpose([coordinates.number;coordinates.x;coordinates.y;coordinates.z]); 
-    
-    save('temp.sfp','sfp','-ascii','-tabs')
-    chanlocs = readlocs('temp.sfp');
-    delete('temp.sfp')
-end
