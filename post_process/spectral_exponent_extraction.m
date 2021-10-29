efd57a5e6e3ff10c87e336b8c519a453220c0f69
Tom Vanasse <thomas.j.vanasse@gmail.com>
2021-10-29 11:52:54 -0500

    push before leaving

--- post_process/spectral_exponent_extraction.m
+++ post_process/spectral_exponent_extraction.m
@@ -1,5 +1,8 @@
 %EEG = pop_loadset('/Volumes/NCCAM/NCCAM/NCCAM3/NCCAM3_Workspace_Backup/wDreamReport/aligned/extraction_TJV/cleaning-sa/post_process/../../sub-2001/eeg/ses-3/awakening-2-cleaned2_nrem.set');
+eeglab
+addpath('spectralExponent');
 
+close all;
 
 % remove alternate path to findpeaks (used in fitPowerLaw)
 rmpath('/Users/tononilab/Documents/MATLAB/eeglab14_1_2b/plugins/PrepPipeline0.55.3/utilities/chronux_2_modified/spectral_analysis/continuous/')
@@ -11,21 +14,17 @@ output = [];
 
 fprintf('Total Files: %d\n',length(T.PATH));
 for file = 1:length(T.PATH)
+% for file = 1:5
     fprintf('Iteration: %d\n',file);
     
     EEG = pop_loadset(char(T.PATH(file)));
-%     if strcmp(char(T.HOLD_OUT(file)),'False')
-%         EEG = pop_loadset(char(T.PATH(file)));
-%     else
-%         continue
-%     end
-
     
-    for timerange = [1, 2] % minutes before awakening
+    for timerange = [0.5, 1] % minutes before awakening
         
         for freqranges = {[1,40], [1 20], [20 40]} % range of frequencies for slope
             
             chanavg = []; % average of spectral exponents
+            chanavg_int = []; % average of intercepts
             obsavg = []; % average of observed psd
             
             for chan = 1:EEG.nbchan % 185 channels
@@ -34,7 +33,6 @@ for file = 1:length(T.PATH)
                  [myPSD,frex]= pwelch( EEG.data(chan,size(EEG.data,2)-timerange*EEG.srate*60:size(EEG.data,2)), ...
                                        epLen, epShift,numFFT, EEG.srate); 
 
-                 %frBand=[1 40];
                  frBand = freqranges{1};
                  frBins= dsearchn( frex, frBand(1) ):  dsearchn( frex, frBand(2));
                  XX= frex(frBins);
@@ -47,14 +45,19 @@ for file = 1:length(T.PATH)
                  thisCol= [0 0 1];
                  [intSlo, stat, Pows, Deviants,  stat0, intSlo0] = fitPowerLaw3steps(XX,YY, robRegMeth,  doPlot, thisCol);
                  spectralExponent= intSlo(2);
+                 intercept=intSlo(1);
+                
                  
                  chanavg = [spectralExponent chanavg];
+                 chanavg_int = [intercept chanavg_int];
                  obsavg = [Pows.obs ; obsavg];
                  
             end
                  
-             %store output in structure
-             data.path = T.PATH(file);
+% %              store output in structure
+             data.path = char(T.PATH(file));
+             data.freqbins = Pows.frex;
+             
              data.chanlocs = EEG.chanlocs(chan);
 %                  data.intSlo = intSlo;
 %                  data.stat = stat;
@@ -64,22 +67,24 @@ for file = 1:length(T.PATH)
              data.intSlo0 = intSlo0;
              data.freqrange = frBand;
              data.timerange = timerange;
+             
                  
              data.Pows = Pows;
              
              data.spectralexp_all = chanavg; % spectral exponent for each channel
+             data.intercept_all = chanavg_int; % intercept of all channels
              data.obs_freq_all = obsavg; % observed frequencies for each channel (matrix)
              
              data.meanspectralexp = mean(chanavg);
+             data.meanintercept = mean(chanavg_int);
              data.obs_freq_avg = mean(obsavg);
 
              output = [data output];
-             save('spectral_slope_output.mat','output');
-
+             
 
         end
     end
 
 end
 
-save('spectral_slope_output.mat','output');
+save('/data/tvanasse/nccam3/data/post_process_data/spectral_slope_output.mat','output');
